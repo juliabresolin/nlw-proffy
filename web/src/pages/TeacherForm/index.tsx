aa05d9316343af938beb16f70b52c4732cd3d5be
jbresolinn <juliabresolin2@gmail.com>
2020-08-03 20:37:31 -0300

    layout: header component

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import PageHeader from '../../components/PageHeader';
 
 function TeacherForm() {
   return (
-    <h1>teacher form works!</h1>
+    <div id="page-teacher-form" className="container">
+    <PageHeader title="Que incrível que você quer dar aulas." description="O primeiro passo, é preencher esse formulário de inscrição."/>
+  </div>
   )
 }
 
